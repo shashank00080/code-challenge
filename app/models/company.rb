be406571590ff3f7caf721cebc234f9560564e6e
shashank00080 <51310391+shashank00080@users.noreply.github.com>
2020-07-18 13:12:39 +0530

    added email validation

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,6 @@
 class Company < ApplicationRecord
   has_rich_text :description
-
+  validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@getmainstreet\.com\z/, message: "must be a @getmainstreet.com account" },
+  allow_blank :true
 
 end
